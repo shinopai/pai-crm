c7da08cc07d317be084e56425505a35ef2c7ddab
shinopai <harbor.shinozaki@gmail.com>
2023-12-29 00:03:56 +0000

    商品のテストデータ作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(UserTableSeeder::class);
+        // $this->call(UserTableSeeder::class);
+        $this->call(ItemTableSeeder::class);
     }
 }
